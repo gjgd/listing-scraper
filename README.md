e69552d022dccbaf3ac06bacfbdd9e6ceb78be23
gjgd <gjgd@protonmail.com>
2022-11-27 20:55:32 -0600

    Add gauge to set taylor prices

---
+++ README.md
@@ -0,0 +1,11 @@
+# Taylor scrapper
+
+```
+python3 -m venv venv
+./venv/bin/pip3 install requests tqdm pandas opentelemetry-exporter-prometheus-remote-write python-dotenv
+./venv/bin/python taylor.py
+```
+
+## Links
+
+- https://github.com/open-telemetry/opentelemetry-python-contrib/blob/main/exporter/opentelemetry-exporter-prometheus-remote-write/example/sampleapp.py#L55
--- taylor.py
+++ taylor.py
@@ -1,7 +1,31 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import time
+
 from listing_scraper import st
+from opentelemetry import metrics
+from opentelemetry.exporter.prometheus_remote_write import (
+    PrometheusRemoteWriteMetricsExporter,
+)
+from opentelemetry.metrics import Observation
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 
 import pandas as pd
 import numpy as np
+
+exporter = PrometheusRemoteWriteMetricsExporter(
+    endpoint=os.getenv("PROM_REMOTE_WRITE_URL"),
+    basic_auth={
+        "username":os.getenv("PROM_REMOTE_WRITE_USER"),
+        "password":os.getenv("PROM_REMOTE_WRITE_PASSWORD"),
+    },
+)
+reader = PeriodicExportingMetricReader(exporter, 60 * 1000)
+provider = MeterProvider(metric_readers=[reader])
+metrics.set_meter_provider(provider)
+meter = metrics.get_meter(__name__)
 
 # https://www.stubhub.com/taylor-swift-arlington-tickets-3-31-2023/event/151219647/
 friday_event_id = 151219647
@@ -10,24 +34,50 @@ saturday_event_id = 150593502
 # https://www.stubhub.com/taylor-swift-arlington-tickets-4-2-2023/event/150593515/
 sunday_event_id = 150593515
 
-events = [friday_event_id, saturday_event_id, sunday_event_id]
-# res = st.get_listings(events)
-# df = pd.concat([pd.DataFrame(r) for r in res])
-
 save_loc = 'taylor.json'
-# df.to_json(save_loc, orient='records')
-df = pd.read_json(save_loc)
 
-min_price = pd.pivot_table(df, values='RawPrice', index=['EventId'], aggfunc=np.min)
-print(min_price)
+def get_taylor_prices_callback(observer):
 
-avg_price = pd.pivot_table(df, values='RawPrice', index=['EventId'], aggfunc=np.mean)
-print(avg_price)
+    events = [friday_event_id, saturday_event_id, sunday_event_id]
 
-max_price = pd.pivot_table(df, values='RawPrice', index=['EventId'], aggfunc=np.max)
-max_price_dict = max_price['RawPrice'].to_dict()
-print(max_price)
+    res = st.get_listings(events)
+    df = pd.concat([pd.DataFrame(r) for r in res])
+    # df.to_json(save_loc, orient='records')
 
-print(max_price_dict[friday_event_id])
-print(max_price_dict[saturday_event_id])
-print(max_price_dict[sunday_event_id])
+    # df = pd.read_json(save_loc)
+
+    print(1)
+    min_price = pd.pivot_table(df, values='RawPrice', index=['EventId'], aggfunc=np.min)
+    min_price_dict = min_price['RawPrice'].to_dict()
+    for event_id, price in min_price_dict.items():
+        labels = {"event_id": event_id, "price": "min_price"}
+        print(labels, price)
+        yield Observation(price, labels)
+
+    print(2)
+    avg_price = pd.pivot_table(df, values='RawPrice', index=['EventId'], aggfunc=np.mean)
+    avg_price_dict = avg_price['RawPrice'].to_dict()
+    for event_id, price in avg_price_dict.items():
+        labels = {"event_id": event_id, "price": "avg_price"}
+        print(labels, price)
+        yield Observation(price, labels)
+
+    print(3)
+    max_price = pd.pivot_table(df, values='RawPrice', index=['EventId'], aggfunc=np.max)
+    max_price_dict = max_price['RawPrice'].to_dict()
+    for event_id, price in max_price_dict.items():
+        labels = {"event_id": event_id, "price": "max_price"}
+        print(labels, price)
+        yield Observation(price, labels)
+
+
+meter.create_observable_counter(
+    callbacks=[get_taylor_prices_callback],
+    name="taylor_prices",
+    description="",
+    unit="",
+)
+
+# while True:
+#   print("hi")
+#   time.sleep(10)
